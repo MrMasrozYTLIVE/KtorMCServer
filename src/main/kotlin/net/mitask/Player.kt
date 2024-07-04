c52fff5aef8f3e94b428d42639dca158f56fd875
MrMasrozYTLIVE <61359286+MrMasrozYTLIVE@users.noreply.github.com>
2024-07-04 11:22:53 +0300

    Started working on some checks for player movement

--- src/main/kotlin/net/mitask/Player.kt
+++ src/main/kotlin/net/mitask/Player.kt
@@ -1,28 +1,47 @@
 package net.mitask
 
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+
 class Player(val playerManager: PlayerManager, val username: String, var posX: Double, var posY: Double, var stance: Double, var posZ: Double, var yaw: Float, var pitch: Float, var onGround: Boolean) {
-    var entityID: Int = 1;
+    var entityID: Int = 1
+    var prevPosX: Double = posX
+    var prevPosY: Double = posY
+    var prevPosZ: Double = posZ
 
     suspend fun updatePosition(x: Double?, y: Double?, stance: Double?, z: Double?, yaw: Float?, pitch: Float?, onGround: Boolean?) {
         if(x != null) {
-//            this.prevXPosition = this.xPosition;
-            this.posX = x;
-        }
-        if(y != null && stance != null) {
-//            this.prevYPosition = this.yPosition;
-            this.posY = y;
-            this.stance = stance;
+            if(x.isNaN() || x.absoluteValue > 3.2E7) return playerManager.kickPlayer("Illegal position")
 
-            val sy = stance - y;
-            if(sy < 0.1 || sy > 1.65) playerManager.kickPlayer("Illegal Stance");
+            this.prevPosX = this.posX
+            this.posX = x
         }
+
+        if(y != null && stance != null) {
+            this.prevPosY = this.posY
+            this.posY = y
+            this.stance = stance
+
+            val sy = stance - y
+            if(sy < 0.1 || sy > 1.65) return playerManager.kickPlayer("Illegal Stance")
+        }
+
         if(z != null) {
-//            this.prevZPosition = this.zPosition;
-            this.posZ = z;
+            if(z.isNaN() || z.absoluteValue > 3.2E7) return playerManager.kickPlayer("Illegal position")
+
+            this.prevPosZ = this.posZ
+            this.posZ = z
         }
-        if(yaw != null) this.yaw = yaw;
-        if(pitch != null) this.pitch = pitch;
-        if(onGround != null) this.onGround = onGround;
+
+        if(yaw != null) this.yaw = yaw
+        if(pitch != null) this.pitch = pitch
+        if(onGround != null) this.onGround = onGround
+
+        if(x != null || z != null) {
+            val distX = (this.posX - this.prevPosX).pow(2)
+            val distZ = (this.posZ - this.prevPosZ).pow(2)
+            if(distX + distZ > 100) return playerManager.kickPlayer("You moved too quickly :( (Hacking?)")
+        }
 
         // console.log(this.toString());
 //        MinecraftServer.sendPacketToAll(PacketEntityTeleport(this.entityID, this.xPosition, this.yPosition, this.zPosition, 0, 0))
